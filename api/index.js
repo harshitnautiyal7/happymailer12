187cfed8ac6d754ca2078c6861d2ca4dc900480f
Harshit Nautiyal <harshitnautiyal7@gmail.com>
2026-02-19 21:44:30 +0530

    final try

--- api/index.js
+++ api/index.js
@@ -88,9 +88,9 @@ if (require.main === module) {
     const PORT = process.env.PORT || 3000;
 
     // In local mode, we need to serve static files
-    app.use(express.static(path.join(__dirname, "../public")));
+    app.use(express.static(path.join(__dirname, "../Public")));
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
+        res.sendFile(path.join(__dirname, "../Public/index.html"));
     });
 
     app.listen(PORT, () => {
